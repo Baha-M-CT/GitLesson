827502eba9959d81f984aa07a948d8ed0c223cfe
bm2701 <bm2701@exo.att.com>
2025-10-01 15:12:03 -0400

    delete

--- test1.java
+++
@@ -1,7 +0,0 @@
-public class test1 {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-        System.out.println("I'm Baha!");
-        System.out.println("We are learning Java!");
-    }
-}
